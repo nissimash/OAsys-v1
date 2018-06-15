0af3056488d801a47d248f35459fd8fc1f56741d
omsachin28 <39898134+omsachin28@users.noreply.github.com>
2018-06-15 19:23:44 +0530

    RFQ Direct Call

--- src/app/magic/RFQ/RFQHeader_WC/RFQHeaderCM.component.ts
+++ src/app/magic/RFQ/RFQHeader_WC/RFQHeaderCM.component.ts
@@ -5,12 +5,13 @@ import { BaseTaskMagicComponent } from "@magic-xpa/angular";
 import {
 	TaskMagicService,
 	ComponentListService,
-	CommandsCollector
+	CommandsCollector, 
+	MgSubformService
 } from "@magic-xpa/angular";
 
 @Component({
 	selector: "mga-RFQHeaderCM",
-	providers: [TaskMagicService],
+	providers: [TaskMagicService, MgSubformService],
 	styleUrls: ["./RFQHeaderCM.component.css"],
 	templateUrl: "./RFQHeaderCM.component.html"
 })
