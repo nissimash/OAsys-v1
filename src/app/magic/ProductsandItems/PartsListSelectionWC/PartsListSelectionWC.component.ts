c80083d78b500aea5d7fbbdca52c53b8aa5b9b90
omsachin28 <39898134+omsachin28@users.noreply.github.com>
2018-06-15 20:04:35 +0530

    PartlistSelection Screen Added

---
+++ src/app/magic/ProductsandItems/PartsListSelectionWC/PartsListSelectionWC.component.ts
@@ -0,0 +1,54 @@
+import {
+    Component
+} from '@angular/core';
+import {
+    Title
+} from '@angular/platform-browser';
+import {
+    BaseModalComponent
+} from "@magic-xpa/angular";;
+import {
+    TaskMagicService,
+    ComponentListService,
+    CommandsCollector,
+    MgSubformService
+} from "@magic-xpa/angular";
+@Component({
+    selector: 'mga-PartsListSelectionWC',
+    providers: [TaskMagicService, MgSubformService],
+    styleUrls: ['./PartsListSelectionWC.component.css'],
+    templateUrl: './PartsListSelectionWC.component.html'
+}) export class PartsListSelectionWC extends BaseModalComponent {
+    private static readonly formName: string = "PartsListSelectionWC";
+    private static readonly showTitleBar: boolean = true;
+    private static readonly x: number = 0;
+    private static readonly y: number = 0;
+    private static readonly width: number = 300;
+    private static readonly height: number = 300;
+    private static readonly isCenteredToWindow: boolean = true;
+    private static readonly shouldCloseOnBackgroundClick = true;
+    get X() {
+        return PartsListSelectionWC.x;
+    }
+    get Y() {
+        return PartsListSelectionWC.y;
+    }
+    get Width() {
+        return PartsListSelectionWC.width;
+    }
+    get Height() {
+        return PartsListSelectionWC.height;
+    }
+    get IsCenteredToWindow() {
+        return PartsListSelectionWC.isCenteredToWindow;
+    }
+    get FormName() {
+        return PartsListSelectionWC.formName;
+    }
+    get ShowTitleBar() {
+        return PartsListSelectionWC.showTitleBar;
+    }
+    get ShouldCloseOnBackgroundClick() {
+        return PartsListSelectionWC.shouldCloseOnBackgroundClick;
+    }
+}
