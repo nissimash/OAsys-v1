c89bd2557448663fbc4aaa228db0f418b5971cea
omsachin28 <39898134+omsachin28@users.noreply.github.com>
2018-06-15 16:56:04 +0530

    Pushing Old  RFQ

--- src/app/magic/RFQ/RFQHeaderList_WC/RFQHeader.component.ts
+++ src/app/magic/RFQ/RFQHeaderList_WC/RFQHeader.component.ts
@@ -1,21 +1,17 @@
+import { Component } from "@angular/core";
+
+import { BaseTaskMagicComponent } from "@magic-xpa/angular";
+
 import {
-    Component
-} from '@angular/core';
-import {
-    Title
-} from '@angular/platform-browser';
-import {
-    BaseTaskMagicComponent
-} from "@magic-xpa/angular";;
-import {
-    TaskMagicService,
-    ComponentListService,
-    CommandsCollector,
-    MgSubformService
+	TaskMagicService,
+	ComponentListService,
+	CommandsCollector
 } from "@magic-xpa/angular";
+
 @Component({
-    selector: 'mga-RFQHeader',
-    providers: [TaskMagicService, MgSubformService],
-    styleUrls: ['./RFQHeader.component.css'],
-    templateUrl: './RFQHeader.component.html'
-}) export class RFQHeader extends BaseTaskMagicComponent {}
+	selector: "mga-RFQHeader",
+	providers: [TaskMagicService],
+	styleUrls: ["./RFQHeader.component.css"],
+	templateUrl: "./RFQHeader.component.html"
+})
+export class RFQHeader extends BaseTaskMagicComponent {}
