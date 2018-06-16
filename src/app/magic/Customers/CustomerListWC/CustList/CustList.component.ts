d748b2240f99cd24aaac3a308c3d54de441ee1cc
Nissim Ashkenazi <nissim.a@gmail.com>
2018-06-16 20:02:47 +0300

    Update ts code with generation from xpa angular versio 146

--- src/app/magic/Customers/CustomerListWC/CustList/CustList.component.ts
+++ src/app/magic/Customers/CustomerListWC/CustList/CustList.component.ts
@@ -1,66 +1,67 @@
-import { Component } from "@angular/core";
-
-import { BaseTaskMagicComponent } from "@magic-xpa/angular";
-
 import {
-	TaskMagicService,
-	ComponentListService,
-	CommandsCollector,
-	MgSubformService
+    Component
+} from '@angular/core';
+import {
+    Title
+} from '@angular/platform-browser';
+import {
+    BaseTaskMagicComponent
+} from "@magic-xpa/angular";;
+import {
+    TaskMagicService,
+    ComponentListService,
+    CommandsCollector,
+    MgSubformService
 } from "@magic-xpa/angular";
-
 import {
-	MatPaginator,
-	MatSort,
-	MatTableDataSource,
-	MatDialog
-} from "@angular/material";
-import { SelectionModel } from "@angular/cdk/collections";
-import { ViewChild } from "@angular/core";
-import { ChangeDetectorRef } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-
+    MatPaginator,
+    MatSort,
+    MatTableDataSource,
+    MatDialog
+} from '@angular/material';
+import {
+    SelectionModel
+} from '@angular/cdk/collections';
+import {
+    ViewChild
+} from "@angular/core";
+import {
+    ChangeDetectorRef
+} from '@angular/core';
+import {
+    Router,
+    ActivatedRoute
+} from '@angular/router';
 @Component({
-	selector: "mga-CustList",
-	providers: [TaskMagicService, MgSubformService],
-	styleUrls: ["./CustList.component.css"],
-	templateUrl: "./CustList.component.html"
-})
-export class CustList extends BaseTaskMagicComponent {
-	@ViewChild(MatPaginator) paginator: MatPaginator;
-	@ViewChild(MatSort) sort: MatSort;
-	displayedColumns = ["Customer Id", "Customer Name", "Address", "Currency"];
-	constructor(
-		public dialog: MatDialog,
-		protected ref: ChangeDetectorRef,
-		public task: TaskMagicService,
-		public mgSub: MgSubformService,
-		protected componentList: ComponentListService
-	) {
-		super(
-			ref,
-			task,
-			mgSub,
-			componentList
-		);
-	}
-	dataSource = new MatTableDataSource<Element>(this.task.Records.list);
-	selection = new SelectionModel<Element>(false, []);
-
-	refreshDataSource() {
-		this.dataSource.data = this.task.Records.list;
-		this.dataSource.paginator = this.paginator;
-	}
-
-	getPageSize(): number {
-		return this.paginator.pageSize;
-	}
-
-	selectRow(rowId: string): void {
-		this.selection.select(this.task.Records.list[rowId]);
-	}
-
-	GetDialog(): any {
-		return this.dialog;
-	}
+    selector: 'mga-CustList',
+    providers: [TaskMagicService, MgSubformService],
+    styleUrls: ['./CustList.component.css'],
+    templateUrl: './CustList.component.html'
+}) export class CustList extends BaseTaskMagicComponent {
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    displayedColumns = ['Customer Id',
+        'Customer Name',
+        'Address',
+        'Currency',
+    ];
+    constructor(public dialog: MatDialog, protected ref: ChangeDetectorRef,
+        public task: TaskMagicService, protected subformService: MgSubformService, protected componentList: ComponentListService, protected titleService: Title) {
+        super(ref, task, subformService, componentList, titleService);
+    }
+    dataSource = new MatTableDataSource < Element > (this.task.Records.list);
+    selection = new SelectionModel < Element > (false, []);
+    refreshDataSource() {
+        this.dataSource.data = this.task.Records.list;
+        this.dataSource.paginator = this.paginator;
+    }
+    getPageSize(): number {
+        return this.paginator.pageSize;
+    }
+    selectRow(rowId: string): void {
+        this.selection.select(this.task.Records.list[rowId]);
+    }
+    GetDialog(): any {
+        return this.dialog;
+    }
 }
