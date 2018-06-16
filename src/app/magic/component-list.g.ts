8d6e33ddf1f2cc2517b05df6b5adf4a48dc19a6c
Nissim Ashkenazi <nissim.a@gmail.com>
2018-06-16 19:58:14 +0300

    Changes for v 146

--- src/app/magic/component-list.g.ts
+++ src/app/magic/component-list.g.ts
@@ -5,65 +5,54 @@ import { PartsListViewWC as PartsListViewWC_PartsListViewWC } from "./Productsan
 import { WLParts as PartsListViewWC_WL_Parts_WLParts } from "./ProductsandItems/PartsListViewWC/WL_Parts/WLParts.component";
 import { RFQHeader as RFQHeaderList_WC_RFQHeader } from "./RFQ/RFQHeaderList_WC/RFQHeader.component";
 import { RFQList as RFQHeaderList_WC_RFQList_RFQList } from "./RFQ/RFQHeaderList_WC/RFQList/RFQList.component";
-
 import { RFQPDF_WC as RFQPDF_WC_RFQPDF_WC } from "./RFQ/RFQPDF_WC/RFQPDF_WC.component";
-
 import { MainScreen as MainScreenWC_MainScreen } from "./Navigation/MainScreenWC/MainScreen.component";
-
 import { RFQHeaderCM as RFQHeader_WC_RFQHeaderCM } from "./RFQ/RFQHeader_WC/RFQHeaderCM.component";
-
 import { RFQDetail as RFQHeader_WC_RFQDetail_RFQDetail } from "./RFQ/RFQHeader_WC/RFQDetail/RFQDetail.component";
-
 import { Attachment as RFQHeader_WC_Attachment_Attachment } from "./RFQ/RFQHeader_WC/Attachment/Attachment.component";
-
 import { CustomerMaster as CustomerListWC_CustomerMaster } from "./Customers/CustomerListWC/CustomerMaster.component";
-
 import { CustList as CustomerListWC_CustList_CustList } from "./Customers/CustomerListWC/CustList/CustList.component";
-
 import { CustomerRegisterWC as CustomerRegister_CustomerRegisterWC } from "./Customers/CustomerRegister/CustomerRegisterWC.component";
-
 import { CustRegList as CustomerRegister_CustRegList_CustRegList } from "./Customers/CustomerRegister/CustRegList/CustRegList.component";
-
 import { CustomerSelectionWC as CustomerSelectionWC_CustomerSelectionWC } from "./Customers/CustomerSelectionWC/CustomerSelectionWC.component";
-
 import { CustomerList as CustomerSelectionWC_CustomerList_CustomerList } from "./Customers/CustomerSelectionWC/CustomerList/CustomerList.component";
+import {
+  DisplayProductConfig as ProdConfig_DisplayProductConfig_DisplayProductConfig
+} from "./ProductConfigurator/ProdConfig/DisplayProductConfig/DisplayProductConfig.component";
+import {
+  DisplaySeriesTypes as ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplaySeriesTypes
+} from "./ProductConfigurator/ProdConfig/DisplayProductConfig/DisplaySeriesTypes/DisplaySeriesTypes.component";
+import {
+  DisplayAttributes as ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayAttributes
+} from "./ProductConfigurator/ProdConfig/DisplayProductConfig/DisplaySeriesTypes/DisplayAttributes/DisplayAttributes.component";
+import {
+  DisplayOptions as ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayOptions_DisplayOptions
+} from "./ProductConfigurator/ProdConfig/DisplayProductConfig/DisplaySeriesTypes/DisplayAttributes/DisplayOptions/DisplayOptions.component";
 
 export const title = "";
 
 export const magicGenCmpsHash = {
-  startupProgram_startupProgram:startupProgram_startupProgram,
-  RoutB_RoutB:RoutB_RoutB,
-  RoutA_RoutA:RoutA_RoutA,
+  startupProgram_startupProgram: startupProgram_startupProgram,
+  RoutB_RoutB: RoutB_RoutB,
+  RoutA_RoutA: RoutA_RoutA,
   PartsListViewWC_PartsListViewWC: PartsListViewWC_PartsListViewWC,
-
   PartsListViewWC_WL_Parts_WLParts: PartsListViewWC_WL_Parts_WLParts,
-
   RFQHeaderList_WC_RFQHeader: RFQHeaderList_WC_RFQHeader,
-
   RFQHeaderList_WC_RFQList_RFQList: RFQHeaderList_WC_RFQList_RFQList,
-
   RFQPDF_WC_RFQPDF_WC: RFQPDF_WC_RFQPDF_WC,
-
   MainScreenWC_MainScreen: MainScreenWC_MainScreen,
-
   RFQHeader_WC_RFQHeaderCM: RFQHeader_WC_RFQHeaderCM,
-
   RFQHeader_WC_RFQDetail_RFQDetail: RFQHeader_WC_RFQDetail_RFQDetail,
-
   RFQHeader_WC_Attachment_Attachment: RFQHeader_WC_Attachment_Attachment,
-
   CustomerListWC_CustomerMaster: CustomerListWC_CustomerMaster,
-
   CustomerListWC_CustList_CustList: CustomerListWC_CustList_CustList,
-
   CustomerRegister_CustomerRegisterWC: CustomerRegister_CustomerRegisterWC,
-
   CustomerRegister_CustRegList_CustRegList: CustomerRegister_CustRegList_CustRegList,
-
   CustomerSelectionWC_CustomerSelectionWC: CustomerSelectionWC_CustomerSelectionWC,
-
-	CustomerSelectionWC_CustomerList_CustomerList: CustomerSelectionWC_CustomerList_CustomerList
-
+  CustomerSelectionWC_CustomerList_CustomerList: CustomerSelectionWC_CustomerList_CustomerList,ProdConfig_DisplayProductConfig_DisplayProductConfig: ProdConfig_DisplayProductConfig_DisplayProductConfig,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplaySeriesTypes: ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplaySeriesTypes,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayAttributes: ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayAttributes,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayOptions_DisplayOptions: ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayOptions_DisplayOptions
 };
 
 export const magicGenComponents = [
@@ -72,30 +61,23 @@ export const magicGenComponents = [
   RoutA_RoutA,
   PartsListViewWC_PartsListViewWC,
   PartsListViewWC_WL_Parts_WLParts,
-
   RFQHeaderList_WC_RFQHeader,
-
   RFQHeaderList_WC_RFQList_RFQList,
-
   RFQPDF_WC_RFQPDF_WC,
-
   MainScreenWC_MainScreen,
-
   RFQHeader_WC_RFQHeaderCM,
-
   RFQHeader_WC_RFQDetail_RFQDetail,
-
   RFQHeader_WC_Attachment_Attachment,
-
   CustomerListWC_CustomerMaster,
-
   CustomerListWC_CustList_CustList,
-
   CustomerRegister_CustomerRegisterWC,
-
   CustomerRegister_CustRegList_CustRegList,
-
   CustomerSelectionWC_CustomerSelectionWC,
-
-	CustomerSelectionWC_CustomerList_CustomerList
+  CustomerSelectionWC_CustomerList_CustomerList,
+  ProdConfig_DisplayProductConfig_DisplayProductConfig,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplaySeriesTypes,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayAttributes,
+  ProdConfig_DisplayProductConfig_DisplaySeriesTypes_DisplayAttributes_DisplayOptions_DisplayOptions
 ];
+
+
