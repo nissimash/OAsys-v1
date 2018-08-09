934ebf2b5192193cb4c40678f23d91cabb44df50
Ankit <asingh@magicsoftawre.com>
2018-08-09 15:52:45 +0530

    fixed button issue under productconfigselection and moved save exit button on top under product configurator..

--- src/app/magic/ProductsandItems/PartsListSelectionWC/PartsListSelectionWC.component.ts
+++ src/app/magic/ProductsandItems/PartsListSelectionWC/PartsListSelectionWC.component.ts
@@ -19,7 +19,7 @@ import {
     private static readonly x: number = 0;
     private static readonly y: number = 0;
     private static readonly width: number = 1100;
-    private static readonly height: number = 750;
+    private static readonly height: number = 580;
     private static readonly isCenteredToWindow: boolean = true;
     private static readonly shouldCloseOnBackgroundClick = false;
      X() {
