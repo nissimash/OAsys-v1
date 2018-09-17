ce62becef63f94b92bc55502bc80fbe9b24932e7
omsachin28 <39898134+omsachin28@users.noreply.github.com>
2018-09-17 13:21:22 +0530

    Combo in place of Text box for "Net"

--- src/app/magic/RFQ/RFQHeader_WC/RFQHeaderCM.component.ts
+++ src/app/magic/RFQ/RFQHeader_WC/RFQHeaderCM.component.ts
@@ -1,9 +1,18 @@
 import { Component } from "@angular/core";
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 import { 
 	TaskBaseMagicComponent,
     magicProviders
 } from "@magic-xpa/angular";
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+	isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+	  const isSubmitted = form && form.submitted;
+	  return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+	}
+  }
 
 @Component({
 	selector: "mga-RFQHeaderCM",
